eb7d7a54c67d3f4dcf52c7426f1af1ccde1d3d02
Norbert Bito <bino104@gmail.com>
2016-08-23 16:00:03 +0200

    first commit

---
+++ src/AuthenticationServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Haematoma\Authentication;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthenticationServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
